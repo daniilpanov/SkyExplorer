7049a62309896a576559a97e8047cccfd73bf441
daniilpanov <daniil_panov2005@mail.ru>
2019-02-25 18:43:06 +0300

    Realised valid drawing BG!

--- src/com/originalstudio/skyexplorer/Game.java
+++ src/com/originalstudio/skyexplorer/Game.java
@@ -13,8 +13,19 @@ public class Game extends JPanel
 	private static final long serialVersionUID = 6693183466444000235L;
 	
 	private StarShip starShip = new StarShip(this);
-	
-	private int bg_x = 0, bg_y = 0;
+	private int bg_w = bg.getWidth(null), bg_h = bg.getHeight(null);
+	private int[][] bg_x =
+				{
+						{-bg_w, 0, bg_w},
+						{-bg_w, 0, bg_w},
+						{-bg_w, 0, bg_w}
+				},
+			bg_y =
+				{
+						{-bg_h, -bg_h, -bg_h},
+						{0, 0, 0},
+						{bg_h, bg_h, bg_h}
+				};
 
 	// 
 	public Game()
@@ -26,7 +37,21 @@ public class Game extends JPanel
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         
-        g.drawImage(bg, bg_x, bg_y, null);
+		int i = 0, j = 0; //
+		do
+		{
+			j++; //
+			//
+			if (j >= 3)
+			{
+				j = 0;
+				i++;
+			}
+			//
+			g.drawImage(bg, bg_x[i][j], bg_y[i][j], null);
+		}
+		//
+		while (i < 2);
         
         starShip.ship.render(g);
     }
